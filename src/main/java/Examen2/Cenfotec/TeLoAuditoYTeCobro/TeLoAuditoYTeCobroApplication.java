d4a5dfc0f92dbf259ab5900eb6a3c6698da554ec
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2022-04-08 19:16:50 -0600

    springboot initializer

---
+++ src/main/java/Examen2/Cenfotec/TeLoAuditoYTeCobro/TeLoAuditoYTeCobroApplication.java
@@ -0,0 +1,13 @@
+package Examen2.Cenfotec.TeLoAuditoYTeCobro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeLoAuditoYTeCobroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeLoAuditoYTeCobroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Examen2/Cenfotec/TeLoAuditoYTeCobro/TeLoAuditoYTeCobroApplicationTests.java
@@ -0,0 +1,13 @@
+package Examen2.Cenfotec.TeLoAuditoYTeCobro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeLoAuditoYTeCobroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
